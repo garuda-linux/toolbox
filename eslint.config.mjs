6d04f20a41ece486467eeecc7e7e198b42ad8dd2
Nico Jensch <root@dr460nf1r3.org>
2026-03-27 12:29:27 +0100

    chore: fix eslint config for newer versions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,10 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
+    languageOptions: {
+      projectService: true,
+      tsconfigRootDir: '.',
+    },
   },
   {
     files: ['**/*.html'],
